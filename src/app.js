0fe6ded3e1c9780715b44542f039e5d6f1c7f0ca
Erick Saravia <ericksaravia16@gmail.com>
2024-04-11 11:31:26 -0600

    feat: Including my resume
    Expose an index.html as static file under "/" path

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+
+app.use(express.static(path.join(__dirname, 'assets')))
 
 app.listen(PORT, () => console.log('Server Running'))
